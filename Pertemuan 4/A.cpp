071747cc057c5155bf491dd0837f6dfda4ba73e3
raflyanandyka <rafly.anandykaa@gmail.com>
2024-09-28 21:27:14 +0700

    added pertemuan 4

---
+++ Pertemuan 4/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// Soal A
+	
+	int A = 5;
+	
+	cout << A+2 << endl;
+	
+	cout << A;
+	
+}
---
+++ Pertemuan 4/B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A, B;
+	
+	A = 5; B = 2;
+	
+	A = B;
+	
+	cout << A << endl;
+	
+	cout << B;
+}
